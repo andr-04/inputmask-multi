8d61699c36224521774e180d14807e5953c4e2f3
Andrey Egorov <andr06@gmail.com>
2013-08-30 11:56:03 +0400

    Plugin doesn't work correctly with jquery.inputmask v2.x yet

--- dev/jquery.inputmask-multi.js
+++ dev/jquery.inputmask-multi.js
@@ -3,7 +3,7 @@
  * https://github.com/andr-04/inputmask-multi
  * Copyright (c) 2012 Andrey Egorov
  * Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
- * Version: 1.0.1
+ * Version: 1.0.2
  *
  * Requriements:
  * https://github.com/RobinHerbots/jquery.inputmask
